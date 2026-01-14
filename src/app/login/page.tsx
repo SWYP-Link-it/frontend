8a1b5e68853816ec741a836b32d0f8e50081aa59
BoeunHan <h.boeunn@gmail.com>
2026-01-14 21:10:05 +0900

    design: 로그인 화면 디자인 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import axios from 'axios';
+import Image from 'next/image';
 
 export default function Login() {
   const handleLogin = () => {
@@ -11,22 +12,40 @@ export default function Login() {
   };
 
   return (
-    <div className="relative flex h-full w-full items-center justify-center">
-      <div className="absolute top-10 left-10">Linkit 로고</div>
-      <div className="flex w-125 flex-col items-center justify-center rounded-lg bg-black px-10 py-20 text-white">
-        <div>Linkit 로고</div>
-        <div className="mt-4 text-[28px] font-bold">메시지 메시지 메시지</div>
-        <div className="mt-2 text-center text-[15px] whitespace-pre-wrap text-[#9599A4]">
-          {`스터디와 사이드 프로젝트를 찾는 가장 쉬운 방법!\nLINKIT에서 함께 할 팀원들을 찾으세요`}
+    <div className="relative flex h-full w-full items-center justify-center bg-[#17171C]">
+      <Image
+        className="absolute top-12.5 left-25"
+        src="/icons/logo.svg"
+        alt="logo"
+        width={111}
+        height={48}
+      />
+      <div className="flex w-120 flex-col items-center gap-[50px] rounded-2xl bg-black px-[50px] py-15 text-white">
+        <div className="flex flex-col items-center gap-[29px]">
+          <Image src="/icons/logo.svg" alt="logo" width={152} height={66} />
+          <div className="flex flex-col items-center gap-4">
+            <div className="text-2xl font-bold">링킷에 오신걸 환영합니다!</div>
+            <div className="text-center text-base font-medium whitespace-pre-wrap text-[#9599A4]">
+              {` 재능을 나누고 필요한 스킬을 배우는 가장 쉬운 방법,\n링킷에서 당신의 스킬을 나눠보세요.`}
+            </div>
+          </div>
         </div>
-        <div
-          className="mt-20 w-full rounded-md bg-[#FEE501] py-2 text-center"
-          onClick={handleLogin}
-        >
-          카카오 계정으로 계속하기
-        </div>
-        <div className="mt-3 w-full rounded-md bg-[#02C75A] py-2 text-center">
-          네이버 계정으로 계속하기
+        <div className="flex w-full flex-col gap-3">
+          <button
+            className="flex h-[58px] w-full cursor-pointer items-center justify-center gap-2 rounded-xl bg-[#FEE501] py-2"
+            onClick={handleLogin}
+          >
+            <Image src="/icons/kakao.svg" alt="kakao" width={24} height={24} />
+            <span className="font-semibold text-black">
+              카카오 계정으로 계속하기
+            </span>
+          </button>
+          <button className="flex h-[58px] w-full cursor-pointer items-center justify-center gap-2 rounded-xl bg-[#02C75A] py-2">
+            <Image src="/icons/naver.svg" alt="naver" width={24} height={24} />
+            <span className="font-semibold text-white">
+              네이버 계정으로 계속하기
+            </span>
+          </button>
         </div>
       </div>
     </div>
