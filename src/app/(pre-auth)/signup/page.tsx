73b6f73ae663cff79b873f18ea47bce0e960904d
BoeunHan <h.boeunn@gmail.com>
2026-01-20 21:59:07 +0900

    feat: 프로필 사진 선택 기능

--- src/app/(pre-auth)/signup/page.tsx
+++ src/app/(pre-auth)/signup/page.tsx
@@ -2,9 +2,13 @@
 
 import { Button } from '@/src/components/Button';
 import { Input } from '@/src/components/Input';
+import { api } from '@/src/lib/api/api';
 import { useState } from 'react';
 
+const MAX_IMAGE_SIZE = 5 * 1024 * 1024;
+
 export default function Signup() {
+  const [file, setFile] = useState<File | null>(null);
   const [nickname, setNickname] = useState('');
   const [showError, setShowError] = useState(false);
   const errorMessage =
@@ -27,14 +31,36 @@ export default function Signup() {
     }
   };
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      if (file.size > MAX_IMAGE_SIZE) {
+        alert('이미지 용량은 5MB 이하만 가능합니다.');
+        return;
+      }
+      setFile(file);
+    }
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append('nickname', nickname);
+    if (file) formData.append('profileImage', file);
+
+    await api.post('/auth/complete-registration', formData);
+  };
+
   return (
     <div className="mx-auto flex h-full w-[490px] flex-col items-center justify-center">
       <div className="mt-10 text-center text-3xl leading-[1.4] font-bold whitespace-pre-wrap">
         {`링킷에서 사용할 프로필을\n등록해주세요.`}
       </div>
-      <div className="border-brand-600 mt-10 h-[200px] w-[300px] border">
-        avatar
-      </div>
+      <input
+        className="border-brand-600 mt-10 h-[200px] w-[300px] cursor-pointer border"
+        type="file"
+        accept="image/jpeg,image/png"
+        onChange={handleFileChange}
+      />
       <div className="mt-10 w-full">
         <Input
           label="닉네임"
@@ -50,6 +76,7 @@ export default function Signup() {
         <Button
           text="회원가입"
           mode={!nickname || errorMessage ? 'inactive' : 'active'}
+          onClick={handleSubmit}
         />
       </div>
     </div>
