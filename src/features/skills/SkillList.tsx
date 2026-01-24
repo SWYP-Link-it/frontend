161a2ac5fc24cc005217ea0e8dcc662a89850367
BoeunHan <h.boeunn@gmail.com>
2026-01-24 17:10:42 +0900

    feat: 리스트 없을 때 화면

--- src/features/skills/SkillList.tsx
+++ src/features/skills/SkillList.tsx
@@ -1,56 +1,74 @@
 import { SkillCard } from './SkillCard';
 
-export const SkillList = () => {
-  const list = [
-    {
-      id: 1,
-      nickname: '홍길동',
-      title: '리액트 기초',
-      description: '리액트 기초를 배워봅시다',
-      rate: 4.5,
-    },
-    {
-      id: 2,
-      nickname: '김철수',
-      title: '디자인 입문',
-      description: '디자인의 기본 개념을 알아봅시다',
-      rate: 4.0,
-    },
-    {
-      id: 3,
-      nickname: '이영희',
-      title: '마케팅 전략',
-      description: '효과적인 마케팅 전략을 수립하는 방법',
-      rate: 4.8,
-    },
-    {
-      id: 4,
-      nickname: '홍길동',
-      title: '리액트 기초',
-      description: '리액트 기초를 배워봅시다',
-      rate: 4.5,
-    },
-    {
-      id: 5,
-      nickname: '김철수',
-      title: '디자인 입문',
-      description: '디자인의 기본 개념을 알아봅시다',
-      rate: 4.0,
-    },
-    {
-      id: 6,
-      nickname: '이영희',
-      title: '마케팅 전략',
-      description:
-        '효과적인 마케팅 전략을 수립하는 방법효과적인 마케팅 전략을 수립하는 방법효과적인 마케팅 전략을 수립하는 방법',
-    },
-  ];
+type SkillListProps = {
+  category?: string;
+};
+
+export const SkillList = ({ category }: SkillListProps) => {
+  const list: any[] = [];
+  // const list = [
+  //   {
+  //     id: 1,
+  //     nickname: '홍길동',
+  //     title: '리액트 기초',
+  //     description: '리액트 기초를 배워봅시다',
+  //     rate: 4.5,
+  //   },
+  //   {
+  //     id: 2,
+  //     nickname: '김철수',
+  //     title: '디자인 입문',
+  //     description: '디자인의 기본 개념을 알아봅시다',
+  //     rate: 4.0,
+  //   },
+  //   {
+  //     id: 3,
+  //     nickname: '이영희',
+  //     title: '마케팅 전략',
+  //     description: '효과적인 마케팅 전략을 수립하는 방법',
+  //     rate: 4.8,
+  //   },
+  //   {
+  //     id: 4,
+  //     nickname: '홍길동',
+  //     title: '리액트 기초',
+  //     description: '리액트 기초를 배워봅시다',
+  //     rate: 4.5,
+  //   },
+  //   {
+  //     id: 5,
+  //     nickname: '김철수',
+  //     title: '디자인 입문',
+  //     description: '디자인의 기본 개념을 알아봅시다',
+  //     rate: 4.0,
+  //   },
+  //   {
+  //     id: 6,
+  //     nickname: '이영희',
+  //     title: '마케팅 전략',
+  //     description:
+  //       '효과적인 마케팅 전략을 수립하는 방법효과적인 마케팅 전략을 수립하는 방법효과적인 마케팅 전략을 수립하는 방법',
+  //   },
+  // ];
 
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {list.map((skill) => (
-        <SkillCard key={skill.id} skill={skill} />
-      ))}
-    </div>
+    <>
+      {list?.length > 0 && (
+        <div className="grid grid-cols-4 gap-4">
+          {list.map((skill) => (
+            <SkillCard key={skill.id} skill={skill} />
+          ))}
+        </div>
+      )}
+      {!list ||
+        (list.length === 0 && (
+          <div className="bg-brand-100 flex flex-1 items-center justify-center gap-3 rounded-xl">
+            <div className="flex flex-col items-center text-gray-400">
+              <span>icon</span>
+              <span className="text-center leading-[1.5] font-semibold whitespace-pre-wrap">{`아직 게시글이 없네요.\n게시글을 올려 게시판에 등록해보세요!`}</span>
+            </div>
+          </div>
+        ))}
+    </>
   );
 };
