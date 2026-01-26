0fc73717c14c2bda61b43e2fca3fd27285598167
BoeunHan <h.boeunn@gmail.com>
2026-01-26 23:47:31 +0900

    feat: 로그인 회원가입 api 연동

--- src/app/(pre-auth)/signup/page.tsx
+++ src/app/(pre-auth)/signup/page.tsx
@@ -3,51 +3,41 @@
 import { Button } from '@/src/components/Button';
 import { Input } from '@/src/components/Input';
 import { api } from '@/src/lib/api/api';
+import { useAuthStore } from '@/src/stores/authStore';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-const MAX_IMAGE_SIZE = 5 * 1024 * 1024;
-
 export default function Signup() {
-  const [file, setFile] = useState<File | null>(null);
+  const router = useRouter();
+  const { setAccessToken } = useAuthStore();
+
   const [nickname, setNickname] = useState('');
-  const [showError, setShowError] = useState(false);
-  const errorMessage =
-    showError && (nickname.length < 1 || nickname.length > 10)
-      ? '닉네임은 최소 1자~10자 이내로 작성 가능해요.'
-      : undefined;
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const allowedRegex = /^[a-zA-Z0-9가-힣]$/;
     const isControlKey =
-      e.ctrlKey ||
-      e.metaKey ||
-      e.key === 'Backspace' ||
-      e.key === 'Delete' ||
-      e.key === 'ArrowLeft' ||
-      e.key === 'ArrowRight';
+      e.ctrlKey || e.metaKey || navigationKeys.includes(e.key);
 
     if (!allowedRegex.test(e.key) && !isControlKey) {
       e.preventDefault();
     }
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      if (file.size > MAX_IMAGE_SIZE) {
-        alert('이미지 용량은 5MB 이하만 가능합니다.');
-        return;
-      }
-      setFile(file);
-    }
-  };
-
   const handleSubmit = async () => {
-    const formData = new FormData();
-    formData.append('nickname', nickname);
-    if (file) formData.append('profileImage', file);
-
-    await api.post('/auth/complete-registration', formData);
+    api
+      .post('/auth/complete-registration', {
+        nickname,
+      })
+      .then((response) => {
+        setAccessToken(response.data.data.accessToken);
+        router.push('/onboarding');
+      })
+      .catch((error) => {
+        // TODO: toast 에러
+        // TODO: 에러 메시지 상세화
+        setErrorMessage('회원가입 실패');
+      });
   };
 
   return (
@@ -55,30 +45,35 @@ export default function Signup() {
       <div className="text-center text-3xl leading-[1.4] font-bold whitespace-pre-wrap">
         {`링킷에서 사용할 프로필을\n등록해주세요.`}
       </div>
-      <input
-        className="border-brand-600 h-[200px] w-[300px] cursor-pointer border"
-        type="file"
-        accept="image/jpeg,image/png"
-        onChange={handleFileChange}
-      />
+      <div className="border-brand-600 h-[200px] w-[300px] border">avatar</div>
       <div className="w-full">
         <Input
           label="닉네임"
           placeholder="사용할 이름을 입력해주세요."
           value={nickname}
           onChange={(e) => setNickname(e.target.value)}
-          onBlur={() => setShowError(true)}
           errorMessage={errorMessage}
           onKeyDown={handleKeyDown}
+          maxLength={10}
         />
       </div>
       <div className="w-[380px]">
         <Button
           text="회원가입"
-          mode={!nickname || errorMessage ? 'inactive' : 'active'}
+          mode={!nickname ? 'inactive' : 'active'}
           onClick={handleSubmit}
         />
       </div>
     </div>
   );
 }
+
+const navigationKeys = [
+  'Backspace',
+  'Delete',
+  'ArrowLeft',
+  'ArrowRight',
+  'Home',
+  'End',
+  'Tab',
+];
--- src/features/auth/LoginContent.tsx
+++ src/features/auth/LoginContent.tsx
@@ -1,26 +1,10 @@
 'use client';
 
-import { api } from '@/src/lib/api/api';
-import { useAuthStore } from '@/src/stores/authStore';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 export const LoginContent = () => {
-  const router = useRouter();
-  const { setAccessToken } = useAuthStore();
-
-  const handleLogin = () => {
-    api
-      .post('/login', {
-        email: 'user@example.com',
-        password: 'password123',
-      })
-      .then((response) => {
-        const { accessToken, refreshToken } = response.data;
-        setAccessToken(accessToken);
-        document.cookie = `refresh_token=${refreshToken}; path=/;`;
-        router.push('/');
-      });
+  const handleLogin = (provider: 'naver' | 'kakao') => {
+    window.location.href = `${process.env.NEXT_PUBLIC_SERVER_URL}/oauth2/authorization/${provider}`;
   };
 
   return (
@@ -37,14 +21,17 @@ export const LoginContent = () => {
       <div className="flex w-[380px] flex-col gap-3">
         <button
           className="flex h-[58px] w-full cursor-pointer items-center justify-center gap-2 rounded-xl bg-[#FEE501] py-2"
-          onClick={handleLogin}
+          onClick={() => handleLogin('kakao')}
         >
           <Image src="/icons/kakao.svg" alt="kakao" width={24} height={24} />
           <span className="font-semibold text-gray-900">
             카카오 계정으로 계속하기
           </span>
         </button>
-        <button className="flex h-[58px] w-full cursor-pointer items-center justify-center gap-2 rounded-xl bg-[#02C75A] py-2">
+        <button
+          className="flex h-[58px] w-full cursor-pointer items-center justify-center gap-2 rounded-xl bg-[#02C75A] py-2"
+          onClick={() => handleLogin('naver')}
+        >
           <Image src="/icons/naver.svg" alt="naver" width={24} height={24} />
           <span className="font-semibold text-white">
             네이버 계정으로 계속하기
