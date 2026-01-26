65756eec4dd17f232343601fade91fc1435c039e
BoeunHan <h.boeunn@gmail.com>
2026-01-26 21:58:34 +0900

    design: 스킬 장터 페이지 css 수정

--- src/app/(main)/skills/page.tsx
+++ src/app/(main)/skills/page.tsx
@@ -10,8 +10,8 @@ export default async function Skills({
   const selectedCategory = (await searchParams).category || '전체';
 
   return (
-    <div className="flex flex-1 flex-col px-28 pb-[126px]">
-      <div className="bg-brand-50 sticky top-[77px]">
+    <div className="flex flex-1 flex-col">
+      <div className="sticky top-[77px] bg-white px-28">
         <div className="my-6 flex flex-col gap-[2px]">
           <h1 className="text-2xl leading-[1.5] font-semibold text-gray-800">
             스킬 게시판
@@ -22,13 +22,15 @@ export default async function Skills({
         </div>
         <SkillCategories category={selectedCategory} />
       </div>
-      <div className="my-7">
-        <CreditInfoBanner />
+      <div className="flex flex-1 flex-col px-28 pb-[126px]">
+        <div className="my-7">
+          <CreditInfoBanner />
+        </div>
+        <span className="text-brand-600 mb-6 w-fit rounded-lg bg-[#F4F2FF] px-3 py-[5px] leading-6 font-semibold">
+          내 크레딧 | 30
+        </span>
+        <SkillList category={selectedCategory} />
       </div>
-      <span className="text-brand-600 mb-6 w-fit rounded-lg bg-[#F4F2FF] px-3 py-[5px] leading-6 font-semibold">
-        내 크레딧 | 30
-      </span>
-      <SkillList category={selectedCategory} />
     </div>
   );
 }
