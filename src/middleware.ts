3412f5254b78d6f4ef7d799be14e5f539dfa14f9
BoeunHan <h.boeunn@gmail.com>
2026-01-27 21:56:43 +0900

    chore: middleware 페이지 보호 주석처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const refresh_token = request.cookies.get('refresh_token')?.value;
-  const isLoggedIn = Boolean(refresh_token);
+  const refreshToken = request.cookies.get('refreshToken')?.value;
+  const isLoggedIn = Boolean(refreshToken);
 
   const protectedPaths = ['/profile'];
   const authPages = ['/login'];
 
-  if (
-    isLoggedIn &&
-    authPages.some((path) => request.nextUrl.pathname.startsWith(path))
-  ) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+  // if (
+  //   isLoggedIn &&
+  //   authPages.some((path) => request.nextUrl.pathname.startsWith(path))
+  // ) {
+  //   return NextResponse.redirect(new URL('/', request.url));
+  // }
 
-  if (
-    !isLoggedIn &&
-    protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))
-  ) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
+  // if (
+  //   !isLoggedIn &&
+  //   protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))
+  // ) {
+  //   return NextResponse.redirect(new URL('/login', request.url));
+  // }
 
   return NextResponse.next();
 }
