d8b5cec083f78dcea22abe8c992c2a68e9a1ff41
BoeunHan <h.boeunn@gmail.com>
2026-01-27 21:59:07 +0900

    feat: 로그인/비로그인 시 GNB 메뉴 다르게

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,15 +1,25 @@
+'use client';
+
 import Link from 'next/link';
-import { MENU_ITEMS } from '../../constants/navigation';
 import { RequiredAuth } from '@/src/features/auth/RequiredAuth';
+import { useIsLoggedIn } from '@/src/stores/selectors';
+import {
+  LOGGED_IN_MENU_ITEMS,
+  LOGGED_OUT_MENU_ITEMS,
+} from '@/src/constants/navigation';
 
 export default function Sidebar() {
+  const isLoggedIn = useIsLoggedIn();
+
+  const menuItems = isLoggedIn ? LOGGED_IN_MENU_ITEMS : LOGGED_OUT_MENU_ITEMS;
+
   return (
     <aside className="flex h-screen w-[80px] flex-col items-center bg-gray-800 py-10">
       <h1 className="text-error-400 mb-10 text-xl font-bold">Linkit</h1>
 
       <nav className="flex flex-col gap-6">
-        {MENU_ITEMS.map((item) => (
-          <RequiredAuth key={item.name}>
+        {menuItems.map((item) => {
+          const link = (
             <Link
               href={item.path}
               className="group hover:text-error-400 flex flex-col items-center gap-2 text-gray-400 transition-colors"
@@ -18,8 +28,14 @@ export default function Sidebar() {
               <div className="group-hover:bg-error-400 h-4 w-4 rounded bg-gray-400" />
               <span className="text-[10px] font-medium">{item.name}</span>
             </Link>
-          </RequiredAuth>
-        ))}
+          );
+
+          return (
+            <div key={item.name}>
+              {item.requiredAuth ? <RequiredAuth>{link}</RequiredAuth> : link}
+            </div>
+          );
+        })}
       </nav>
     </aside>
   );
