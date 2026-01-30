a6d35a37654efbead13b4d65ceef1e45ff4c0ff9
BoeunHan <h.boeunn@gmail.com>
2026-01-30 13:04:38 +0900

    design: MessageIcon svg 수정

--- src/components/icons/MessageIcon.tsx
+++ src/components/icons/MessageIcon.tsx
@@ -10,14 +10,27 @@ export const MessageIcon = ({ size = 16, className }: SVGProps) => {
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
-      <circle cx="10.0001" cy="9.99987" r="7.99987" fill="currentColor" />
+      <circle
+        cx="10.0001"
+        cy="9.99987"
+        r="7.99987"
+        fill="currentColor"
+        mask="url(#dots-cutout)"
+      />
       <path
         d="M10.0952 17.9984C2.37808 18.138 0.830309 15.7839 1.81308 15.6266C4.82469 15.1446 3.32521 12.1187 5.35627 14.1176C6.36624 15.1116 11.5231 17.9725 10.0952 17.9984Z"
         fill="currentColor"
       />
-      <circle cx="5.96468" cy="10.0006" r="1.07186" fill="#E9EAEB" />
-      <circle cx="9.99984" cy="10.0006" r="1.07186" fill="#E9EAEB" />
-      <circle cx="14.0347" cy="10.0006" r="1.07186" fill="#E9EAEB" />
+      <defs>
+        <mask id="dots-cutout">
+          <rect width="100%" height="100%" fill="white" />
+
+          {/* 구멍 뚫을 부분 */}
+          <circle cx="5.96468" cy="10.0006" r="1.07186" fill="black" />
+          <circle cx="9.99984" cy="10.0006" r="1.07186" fill="black" />
+          <circle cx="14.0347" cy="10.0006" r="1.07186" fill="black" />
+        </mask>
+      </defs>
     </svg>
   );
 };
