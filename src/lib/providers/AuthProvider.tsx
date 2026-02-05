8d449900bc28251a7482319382537b21b41a370a
BoeunHan <h.boeunn@gmail.com>
2026-02-05 23:12:38 +0900

    fix: axios interceptor refresh 요청 로직 개선 및 중복 요청 방지

--- src/lib/providers/AuthProvider.tsx
+++ src/lib/providers/AuthProvider.tsx
@@ -1,11 +1,19 @@
-import { useAuthStore } from '@/src/stores/authStore';
+import { useUserInfoStore } from '@/src/stores/userInfoStore';
 import { ReactNode, useEffect } from 'react';
+import { api } from '../api/api';
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const refresh = useAuthStore((state) => state.refresh);
+  const setUserInfo = useUserInfoStore((state) => state.setUserInfo);
+
+  const fetchUserInfo = () => {
+    api.get(`/credits/balance-user-details`).then((response) => {
+      const { userId, userNickname, creditBalance } = response.data.data;
+      setUserInfo({ userId, userNickname, creditBalance });
+    });
+  };
 
   useEffect(() => {
-    refresh();
+    fetchUserInfo();
   }, []);
   return children;
 };
--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,11 +1,9 @@
 import { create } from 'zustand';
-import axios from 'axios';
 
 interface AuthState {
   accessToken: string | null;
   setAccessToken: (accessToken: string) => void;
   clearAccessToken: () => void;
-  refresh: () => void;
 }
 
 export const useAuthStore = create<AuthState>()((set) => ({
@@ -17,24 +15,5 @@ export const useAuthStore = create<AuthState>()((set) => ({
 
   clearAccessToken: () => {
     set({ accessToken: null });
-  },
-
-  refresh: () => {
-    axios
-      .post(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/refresh`,
-        {},
-        {
-          withCredentials: true,
-        },
-      )
-      .then((response) => {
-        const newAccessToken = response.data.data.accessToken;
-        set({ accessToken: newAccessToken });
-      })
-      .catch((error) => {
-        console.error('토큰 갱신 실패:', error);
-        set({ accessToken: null });
-      });
   },
 }));
