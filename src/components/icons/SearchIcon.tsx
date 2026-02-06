0c71b12aef633f044bfce75957b2f6cf28e50763
BoeunHan <h.boeunn@gmail.com>
2026-02-06 23:36:56 +0900

    fix: svg 컴포넌트 속성에러 수정

--- src/components/icons/SearchIcon.tsx
+++ src/components/icons/SearchIcon.tsx
@@ -13,9 +13,9 @@ export const SearchIcon = ({ size = 16, className }: SVGProps) => {
       <path
         d="M12.1373 12.1373L9.62312 9.62312M10.9813 6.35762C10.9813 8.91123 8.91123 10.9813 6.35762 10.9813C3.804 10.9813 1.73389 8.91123 1.73389 6.35762C1.73389 3.804 3.804 1.73389 6.35762 1.73389C8.91123 1.73389 10.9813 3.804 10.9813 6.35762Z"
         stroke="currentColor"
-        stroke-width="1.15593"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.15593"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
