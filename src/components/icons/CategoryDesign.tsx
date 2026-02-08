595c3110da19840058bb924b62ac7facee1de65a
BoeunHan <h.boeunn@gmail.com>
2026-02-08 22:01:45 +0900

    chore: svg 클래스명 수정

--- src/components/icons/CategoryDesign.tsx
+++ src/components/icons/CategoryDesign.tsx
@@ -29,30 +29,30 @@ export const CategoryDesign = ({ size = 16, className }: SVGProps) => {
       <path
         d="M4 24C4 35.0457 12.9543 44 24 44C27.3137 44 30 41.3137 30 38V37C30 36.0712 30 35.6067 30.0513 35.2168C30.4058 32.5244 32.5244 30.4058 35.2168 30.0513C35.6067 30 36.0712 30 37 30H38C41.3137 30 44 27.3137 44 24C44 12.9543 35.0457 4 24 4C12.9543 4 4 12.9543 4 24Z"
         stroke="currentColor"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M14 26C15.1046 26 16 25.1046 16 24C16 22.8954 15.1046 22 14 22C12.8954 22 12 22.8954 12 24C12 25.1046 12.8954 26 14 26Z"
         stroke="currentColor"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M32 18C33.1046 18 34 17.1046 34 16C34 14.8954 33.1046 14 32 14C30.8954 14 30 14.8954 30 16C30 17.1046 30.8954 18 32 18Z"
         stroke="currentColor"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20 16C21.1046 16 22 15.1046 22 14C22 12.8954 21.1046 12 20 12C18.8954 12 18 12.8954 18 14C18 15.1046 18.8954 16 20 16Z"
         stroke="currentColor"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <circle cx="20" cy="14" r="3" fill="#E9EAEB" />
       <circle cx="32" cy="16" r="3" fill="#E9EAEB" />
