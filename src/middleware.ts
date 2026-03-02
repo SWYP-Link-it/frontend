ab6c72e10bb68c36c938ec9a5d49c273f1ba5d7e
BoeunHan <h.boeunn@gmail.com>
2026-03-03 01:27:52 +0900

    feat: 쿠키에 저장된 refreshToken 기반 라우트 보호

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,17 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const refreshToken = request.cookies.get('refreshToken')?.value;
+
+  console.log('[Middleware] refreshToken:', refreshToken);
   const isLoggedIn = Boolean(refreshToken);
 
-  const protectedPaths = ['/profile'];
-  const authPages = ['/login'];
+  const protectedPaths = [
+    '/profile',
+    '/messages',
+    '/requests',
+    '/skills/request',
+  ];
+  const authPages = ['/login', '/signup', '/auth'];
 
   const { pathname } = request.nextUrl;
 
@@ -13,23 +20,32 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/skills', request.url));
   }
 
-  // if (
-  //   isLoggedIn &&
-  //   authPages.some((path) => request.nextUrl.pathname.startsWith(path))
-  // ) {
-  //   return NextResponse.redirect(new URL('/', request.url));
-  // }
+  if (
+    isLoggedIn &&
+    authPages.some((path) => request.nextUrl.pathname.startsWith(path))
+  ) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
 
-  // if (
-  //   !isLoggedIn &&
-  //   protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))
-  // ) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  if (
+    !isLoggedIn &&
+    protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))
+  ) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/profile', '/login', '/'],
+  matcher: [
+    '/profile/:path*',
+    '/messages/:path*',
+    '/requests/:path*',
+    '/skills/request',
+    '/auth',
+    '/login',
+    '/signup',
+    '/',
+  ],
 };
