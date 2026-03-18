3ae34aad3e6e91d0cbc6311f672d46321853da7d
BoeunHan <h.boeunn@gmail.com>
2026-03-18 22:12:26 +0900

    chore: z index 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -14,7 +14,7 @@ export default function MainLayout({
         className="pointer-events-none absolute inset-0 left-20 z-0 bg-[radial-gradient(54.47%_52.89%_at_50%_65%,rgba(108,214,245,0.2)_0%,rgba(254,226,253,0.2)_100%)]"
       />
       <Sidebar />
-      <main className="relative z-10 flex h-full w-full flex-col overflow-y-auto">
+      <main className="relative flex h-full w-full flex-col overflow-y-auto">
         <Suspense fallback={null}>
           <Header />
         </Suspense>
